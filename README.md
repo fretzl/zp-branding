137ac9938e5b25e00cc211220b07e1763b552260
fretzl <fred@zenphoto.org>
2025-10-18 12:49:25 +0200

    Major overhaul of the plugin. Now version 2.0

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 zp-branding
 ===========
 
-A Zenphoto plugin to replace the default Zenphoto logo on the backend with a custom logo.
+A Zenphoto plugin to replace the default Zenphoto logo on the backend with a custom logo and the possibility to use custom CSS to change the appearance of the logo and/or backend.
 
-Create a PNG image with filename <code>zp-admin-logo.png</code> and replace the example file with the same name in the <code>/plugins/zp-branding/</code> folder.
+Version 2.0 is a combination of the 1.4 version and kuzzzma's admin-branding plugin (https://github.com/kuz-z-zma/adminBranding)
+
+
+**Options:**
+
+ - Select *No logo*, *Default logo* or *Custom logo*.
+ - Set width, height is proportional.
+ - Add custom inline CSS to change the appearance of the logo and/or backend.
+ - Select the custom logo you want to use from the */uploaded/adminlogo/* folder.
+ - Supports translations.
+
+
+**Installation:**
  
-The default size of your logo will be the original size of your image.
+ 1. Copy "/zp-branding" folder and "zp-branding.php" into the */plugins/* folder of your Zenphoto installation.
+2. Enable the "zp-branding" plugin on the backend.
+3. Upload custom logo images into */uploaded/adminlogo/* folder (see above).
+4. Set plugin options as you wish.
+ 
 
-You can change the width in the options.
-
-Since v1.4 there is an option to add custom CSS to alter the appearance of the backend.
--- zp-branding.php
+++ zp-branding.php
@@ -2,45 +2,73 @@
 /**
  * Replacement of the Zenphoto logo on the backend with a custom logo.
  *
- * Name the logo <code>zp-admin-logo.png</code> and place it in the <code>/plugins/zp-branding</code> folder.
+ * This plugin is a combination of the <code>admin-branding</code> plugin from kuzzzma and my <code>zp-branding</code> plugin.
  *
- * @author Fred Sondaar (fretzl)
+ * Create a folder with the name <em>adminlogo</em> in the <em>uploaded</em> folder or use elFinder to do so.
+ * Upload the file(s) to the <em>adminlogo</em> folder you wish to use as a custom backend logo.
+ * Set options as desired.
+ *
+ * ## Options:
+ *
+ * - Select what you want to see. No logo, the Zenphoto default logo or a custom logo.
+ * - Set the width of the logo. Height is proportional.
+ * - Add custom CSS to change the appearance of the logo and/or backend.
+ * - Select the custom logo in the <em>/uploaded/adminlogo/</em> folder you want to use.
+ *
+ * @author Fred Sondaar (fretzl), with a lot of additions from kuzzma's <em>admin-branding</em> ver. 1.1 plugin (https://github.com/kuz-z-zma/adminBranding)
  * @package plugins
  * @subpackage admin
  */
 
 $plugin_is_filter = 5|ADMIN_PLUGIN;
 $plugin_description = gettext_pl("Replace the default Zenphoto logo on the backend with a custom logo.", 'zp-branding');
-$plugin_author = "Fred Sondaar (fretzl)";
-$plugin_version = '1.4';
-$plugin_category = gettext('Admin');
+$plugin_author = "Fred Sondaar (fretzl), with additions from kuzzzma's <em>admin-branding</em> ver. 1.1 plugin (https://github.com/kuz-z-zma/adminBranding)";
+$plugin_siteurl = 'https://github.com/fretzl/zp-branding';
+$plugin_version = '2.0.0';
+$plugin_disable = (zp_has_filter('admin_head') && extensionEnabled('admin-branding')) ? gettext_pl('Only one Zenphoto backend customization plugin may be enabled. Please disable the <code>admin-branding</code> plugin to use this one.','zp-branding') : '';
+$plugin_category = gettext_pl('Admin', 'zp-branding');
 $option_interface = 'zpBrandingOptions';
 
-zp_register_filter('admin_head', 'zpBranding::printCustomZpLogo');
+if ($plugin_disable) {
+    enableExtension('zp-branding', 0);
+} else {
+    zp_register_filter('admin_head', 'zpBranding::printCustomZpLogo');
+}
 
-$zp_branding_logo = FULLWEBPATH . '/' . USER_PLUGIN_FOLDER . '/zp-branding/zp-admin-logo.png';
+$zp_branding_logo = FULLWEBPATH . '/' . ZENFOLDER . '/images/zen-logo.png';
 
 class zpBrandingOptions {
 	
 	function __construct() {
 		purgeOption('width');// older version option name
 		purgeOption('restore');// older version option name
+		setOptionDefault('zpbranding_logo-image', 'default');
 		setOptionDefault('zpbranding-width', '200');
-		setThemeOptionDefault('zpbranding_customcss', '');
+		setOptionDefault('zpbranding_custom_css', '');
 	}
 
 	static function getOptionsSupported() {
 		global $zp_branding_logo;
 		if ( $zp_branding_logo ) {
 			$width = getimagesize($zp_branding_logo)[0];
-			$options = array(    
+			$options = array( 
+					gettext_pl('Logo for backend', 'zp-branding') => array('key' => 'zpbranding_logo-image', 'type' => OPTION_TYPE_RADIO,
+						'order' => 1,
+						'buttons' => array(
+							gettext_pl('No logo', 'zp-branding') => 'disabled',
+							gettext_pl('Custom logo', 'zp-branding') => 'custom',
+							gettext_pl('Default Zenphoto logo', 'zp-branding') => 'default'),
+						'desc' => gettext_pl('Choose what you want to use as logo or no logo at all.', 'zp-branding')),   
+					gettext_pl('Select admin logo image', 'zp-branding') => array('key' => 'zpbranding_logo-custom', 'type' => OPTION_TYPE_CUSTOM, 
+						'order' => 4, 
+						'desc' => sprintf(gettext_pl('When <em>Custom logo</em> is selected for <em>Logo for backend</em> you may select a logo from files in the <em>%s</em> folder.<br>If you use the <em>elFinder</em> plugin for Uploads, you can upload files to this folder. Alternatively, you can use FTP to upload your image to the <em>adminlogo</em> folder.', 'zp-branding'),(UPLOAD_FOLDER.'/adminlogo/'))),
 					gettext_pl('Width', 'zp-branding') => array('key' => 'zpbranding-width', 'type' => OPTION_TYPE_TEXTBOX,
 						'order'=> 1,
-						'desc' => gettext_pl('The width of the image (px). The height is proportional.', 'zp-branding')),
-					gettext_pl('Custom CSS', 'zp-branding') => array('key' => 'zpbranding_customcss', 'type' => OPTION_TYPE_TEXTAREA, 
+						'desc' => gettext_pl('The width of the logo (px). Default is 200px.<br>The height is proportional.', 'zp-branding')),
+					gettext_pl('Custom CSS', 'zp-branding') => array('key' => 'zpbranding_custom_css', 'type' => OPTION_TYPE_TEXTAREA, 
 						'order' => 3,
 						'multilingual' => 0,
-						'desc' => gettext_pl('Enter custom CSS to alter the appearance of the admin area.<br> It is printed between &lt;style&gt; tags in the &lt;head&gt; section.', 'zp-branding'))
+						'desc' => gettext_pl('Enter custom CSS to alter the appearance of the admin area.<br>It is printed between &lt;style&gt; tags in the &lt;head&gt; section.', 'zp-branding'))
 				);
 				if ( getOption('zpbranding-width') != $width ) {
 					$options[gettext_pl('Reset', 'zp-branding')] = array('key' => 'zpbranding-restore', 'type' => OPTION_TYPE_CHECKBOX,
@@ -56,6 +84,16 @@ class zpBrandingOptions {
 		}
 	}
 	
+	function handleOption($option, $currentValue) {
+        if ( $option == "zpbranding_logo-custom" ) { ?>
+            <select id="zpbranding_logo-custom" name="zpbranding_logo-custom">
+                <option value="" style="background-color:LightGray"><?php echo gettext_pl('*Not specified*', 'zp-branding'); ?></option>';
+                <?php zp_apply_filter('theme_head');
+                generateListFromFiles($currentValue, SERVERPATH.'/'.UPLOAD_FOLDER.'/adminlogo/','');	?>
+            </select>
+            <?php }
+    }
+
 	function handleOptionSave() {
 		global $zp_branding_logo;
 		$width = getimagesize($zp_branding_logo)[0];
@@ -70,6 +108,9 @@ class zpBranding {
 
 	static function printCustomZpLogo() {
 		global $zp_branding_logo;
+		if ( getOption('zpbranding_logo-image') == 'custom' && getOption('zpbranding_logo-custom') != '' ) {
+            $$zp_branding_logo = FULLWEBPATH.'/'.UPLOAD_FOLDER.'/adminlogo/' . getOption('zpbranding_logo-custom');
+        }
 		if (getimagesize($zp_branding_logo)) {// Check if file is image
 			$width = getimagesize($zp_branding_logo)[0];
 			$height = getimagesize($zp_branding_logo)[1];
@@ -84,32 +125,49 @@ class zpBranding {
 				setOption('zpbranding-width', $width);
 			}
 			?>
-			<style>
-			#logo {
-				display: none;
-			}
-		
-			#administration {
-				width: <?php echo $new_width; ?>px;
-				height: <?php echo $height; ?>px;
-				margin: 20px 10px 0px 30px;
-				background: url(<?php echo $zp_branding_logo; ?>) no-repeat 0 0;
-				background-size: <?php echo $new_width; ?>px;
-			}
+			
 			<?php
-			if ( !empty(getOption('zpbranding_customcss')) ) {
-				echo "\n/** Custom CSS **/\n" . getOption('zpbranding_customcss') . "\n" . "/****************/" . "\n";
-			}
+			$custom_css = '';
+			if ( !empty(getOption('zpbranding_custom_css')) ) {
+				$custom_css = "\n/**----------- Custom CSS -----------**/\n" . getOption('zpbranding_custom_css') . "\n" . "/*-----------End of Custom CSS-----------------*/" . "\n";
+			} 
 			?>
-			</style>
-			<script>
-			document.addEventListener('DOMContentLoaded', function() { 
-				if ( document.getElementById('logo') ) {
-					document.getElementById('logo').remove();
-				}; 
-			});
-			</script>
+			
+			<?php 
+			if ( getOption('zpbranding_logo-image') == 'disabled' ) { ?>
+				<style>
+					#logo {
+						display: none; 
+					}
+
+					<?php echo $custom_css; ?>
+				</style>
+			<?php } ?>
+				
 			<?php
+			if ( getOption('zpbranding_logo-image') == 'default' ) { ?>
+				<style>
+					<?php echo $custom_css; ?>
+				</style>
+			<?php } ?>
+			
+			<?php 
+			if ( getOption('zpbranding_logo-image') == 'custom' && getOption('zpbranding_logo-custom') != '' ) { ?>
+				<style>
+					#logo {
+						display: none;
+					}
+					
+					#administration {
+						width: <?php echo $new_width; ?>px;
+						height: <?php echo $height; ?>px;
+						background: url("<?php echo pathurlencode(WEBPATH.'/'.UPLOAD_FOLDER.'/adminlogo/' . getOption('zpbranding_logo-custom')); ?>") no-repeat 0 0;
+						background-size: <?php echo $new_width; ?>px;
+					}
+					
+					<?php echo $custom_css; ?>
+				</style>
+			<?php }
 		} else { ?>
 			<div class="errorbox">
 			<?php echo sprintf(gettext_pl("Image <i>%s</i> does not exist.", 'zp-branding'), substr($zp_branding_logo, strrpos($zp_branding_logo, '/') + 1)); ?>
