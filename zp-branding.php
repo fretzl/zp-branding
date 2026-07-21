b24223dc5a57f389664cee8a5d83b150a526e0f1
fretzl <fred@zenphoto.org>
2026-07-21 21:05:29 +0200

    Aply new available methods

--- zp-branding.php
+++ zp-branding.php
@@ -21,18 +21,18 @@
  */
 
 $plugin_is_filter = 5|ADMIN_PLUGIN;
-$plugin_description = gettext_pl("Replace the default Zenphoto logo on the backend with a custom logo.", 'zp-branding');
+$plugin_description = i18n::gettext_pl("Replace the default Zenphoto logo on the backend with a custom logo.", 'zp-branding');
 $plugin_author = "Fred Sondaar (fretzl), with additions from kuzzzma's <em>admin-branding</em> ver. 1.1 plugin (https://github.com/kuz-z-zma/adminBranding)";
 $plugin_siteurl = 'https://github.com/fretzl/zp-branding';
 $plugin_version = '2.0.0';
-$plugin_disable = (zp_has_filter('admin_head') && extensionEnabled('admin-branding')) ? gettext_pl('Only one Zenphoto backend customization plugin may be enabled. Please disable the <code>admin-branding</code> plugin to use this one.','zp-branding') : '';
-$plugin_category = gettext_pl('Admin', 'zp-branding');
+$plugin_disable = (filter::hasFilter('admin_head') && extensionEnabled('admin-branding')) ? i18n::gettext_pl('Only one Zenphoto backend customization plugin may be enabled. Please disable the <code>admin-branding</code> plugin to use this one.','zp-branding') : '';
+$plugin_category = i18n::gettext_pl('Admin', 'zp-branding');
 $option_interface = 'zpBrandingOptions';
 
 if ($plugin_disable) {
     enableExtension('zp-branding', 0);
 } else {
-    zp_register_filter('admin_head', 'zpBranding::printCustomZpLogo');
+    filter::registerFilter('admin_head', 'zpBranding::printCustomZpLogo');
 }
 
 $zp_branding_logo = FULLWEBPATH . '/' . ZENFOLDER . '/images/zen-logo.png';
@@ -52,33 +52,33 @@ class zpBrandingOptions {
 		if ( $zp_branding_logo ) {
 			$width = getimagesize($zp_branding_logo)[0];
 			$options = array( 
-					gettext_pl('Logo for backend', 'zp-branding') => array('key' => 'zpbranding_logo-image', 'type' => OPTION_TYPE_RADIO,
+					i18n::gettext_pl('Logo for backend', 'zp-branding') => array('key' => 'zpbranding_logo-image', 'type' => OPTION_TYPE_RADIO,
 						'order' => 1,
 						'buttons' => array(
-							gettext_pl('No logo', 'zp-branding') => 'disabled',
-							gettext_pl('Custom logo', 'zp-branding') => 'custom',
-							gettext_pl('Default Zenphoto logo', 'zp-branding') => 'default'),
-						'desc' => gettext_pl('Choose what you want to use as logo or no logo at all.', 'zp-branding')),   
-					gettext_pl('Select admin logo image', 'zp-branding') => array('key' => 'zpbranding_logo-custom', 'type' => OPTION_TYPE_CUSTOM, 
+							i18n::gettext_pl('No logo', 'zp-branding') => 'disabled',
+							i18n::gettext_pl('Custom logo', 'zp-branding') => 'custom',
+							i18n::gettext_pl('Default Zenphoto logo', 'zp-branding') => 'default'),
+						'desc' => i18n::gettext_pl('Choose what you want to use as logo or no logo at all.', 'zp-branding')),   
+					i18n::gettext_pl('Select admin logo image', 'zp-branding') => array('key' => 'zpbranding_logo-custom', 'type' => OPTION_TYPE_CUSTOM, 
 						'order' => 4, 
-						'desc' => sprintf(gettext_pl('When <em>Custom logo</em> is selected for <em>Logo for backend</em> you may select a logo from files in the <em>%s</em> folder.<br>If you use the <em>elFinder</em> plugin for Uploads, you can upload files to this folder. Alternatively, you can use FTP to upload your image to the <em>adminlogo</em> folder.', 'zp-branding'),(UPLOAD_FOLDER.'/adminlogo/'))),
-					gettext_pl('Width', 'zp-branding') => array('key' => 'zpbranding-width', 'type' => OPTION_TYPE_TEXTBOX,
+						'desc' => sprintf(i18n::gettext_pl('When <em>Custom logo</em> is selected for <em>Logo for backend</em> you may select a logo from files in the <em>%s</em> folder.<br>If you use the <em>elFinder</em> plugin for Uploads, you can upload files to this folder. Alternatively, you can use FTP to upload your image to the <em>adminlogo</em> folder.', 'zp-branding'),(UPLOAD_FOLDER.'/adminlogo/'))),
+					i18n::gettext_pl('Width', 'zp-branding') => array('key' => 'zpbranding-width', 'type' => OPTION_TYPE_TEXTBOX,
 						'order'=> 1,
-						'desc' => gettext_pl('The width of the logo (px). Default is 200px.<br>The height is proportional.', 'zp-branding')),
-					gettext_pl('Custom CSS', 'zp-branding') => array('key' => 'zpbranding_custom_css', 'type' => OPTION_TYPE_TEXTAREA, 
+						'desc' => i18n::gettext_pl('The width of the logo (px). Default is 200px.<br>The height is proportional.', 'zp-branding')),
+					i18n::gettext_pl('Custom CSS', 'zp-branding') => array('key' => 'zpbranding_custom_css', 'type' => OPTION_TYPE_TEXTAREA, 
 						'order' => 3,
 						'multilingual' => 0,
-						'desc' => gettext_pl('Enter custom CSS to alter the appearance of the admin area.<br>It is printed between &lt;style&gt; tags in the &lt;head&gt; section.', 'zp-branding'))
+						'desc' => i18n::gettext_pl('Enter custom CSS to alter the appearance of the admin area.<br>It is printed between &lt;style&gt; tags in the &lt;head&gt; section.', 'zp-branding'))
 				);
 				if ( getOption('zpbranding-width') != $width ) {
-					$options[gettext_pl('Reset', 'zp-branding')] = array('key' => 'zpbranding-restore', 'type' => OPTION_TYPE_CHECKBOX,
+					$options[i18n::gettext_pl('Reset', 'zp-branding')] = array('key' => 'zpbranding-restore', 'type' => OPTION_TYPE_CHECKBOX,
 						'order' => 2,
-						'desc' => gettext_pl('Reset to the original width.', 'zp-branding'));
+						'desc' => i18n::gettext_pl('Reset to the original width.', 'zp-branding'));
 				}			
 		return $options;
 		} else { ?>
 			<div class="errorbox">
-			<?php echo sprintf(gettext_pl("Image <i>%s</i> does not exist.", 'zp-branding'), substr($zp_branding_logo, strrpos($zp_branding_logo, '/') + 1)); ?>
+			<?php echo sprintf(i18n::gettext_pl("Image <i>%s</i> does not exist.", 'zp-branding'), substr($zp_branding_logo, strrpos($zp_branding_logo, '/') + 1)); ?>
 			</div>
 		<?php
 		}
@@ -87,8 +87,8 @@ class zpBrandingOptions {
 	function handleOption($option, $currentValue) {
         if ( $option == "zpbranding_logo-custom" ) { ?>
             <select id="zpbranding_logo-custom" name="zpbranding_logo-custom">
-                <option value="" style="background-color:LightGray"><?php echo gettext_pl('*Not specified*', 'zp-branding'); ?></option>';
-                <?php zp_apply_filter('theme_head');
+                <option value="" style="background-color:LightGray"><?php echo i18n::gettext_pl('*Not specified*', 'zp-branding'); ?></option>';
+                <?php filter::applyFilter('theme_head');
                 generateListFromFiles($currentValue, SERVERPATH.'/'.UPLOAD_FOLDER.'/adminlogo/','');	?>
             </select>
             <?php }
@@ -170,7 +170,7 @@ class zpBranding {
 			<?php }
 		} else { ?>
 			<div class="errorbox">
-			<?php echo sprintf(gettext_pl("Image <i>%s</i> does not exist.", 'zp-branding'), substr($zp_branding_logo, strrpos($zp_branding_logo, '/') + 1)); ?>
+			<?php echo sprintf(i18n::gettext_pl("Image <i>%s</i> does not exist.", 'zp-branding'), substr($zp_branding_logo, strrpos($zp_branding_logo, '/') + 1)); ?>
 			</div>
 		<?php
 		}
